f88d2152d3cf4c17e6722271c6cf247f743f1e41
Natalia Telpukhova and Samuel Gespass <natalia+samuel@>
2018-01-02 16:39:43 -0800

    add user interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 /**
  * Created by Guest on 1/2/18.
  */
 public class App {
+    public static void main(String[] args) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Anagrams: Write some words");
+            String input = bufferedReader.readLine();
+            Anagrams newAnagrans = new Anagrams();
+            String output = newAnagrans.multiple(input);
+            System.out.println(output);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
